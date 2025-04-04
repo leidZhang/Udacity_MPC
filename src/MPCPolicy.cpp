56be5ed7685e011a938a3600bff14021a3bb7c5e
leidZhang <haoz28889@gmail.com>
2025-04-03 20:16:49 -0400

    waypoint format last year

--- src/MPCPolicy.cpp
+++ src/MPCPolicy.cpp
@@ -13,7 +13,7 @@ using std::vector;
 namespace py = pybind11;
 
 // Make sure use the same name in CMakeLists
-PYBIND11_MODULE(mpc, m) { 
+PYBIND11_MODULE(mpc, m) {
     py::class_<MPCPolicy>(m, "MPCPolicy")
         .def(py::init<>())  // constructor
         .def("reset", &MPCPolicy::reset)
@@ -38,27 +38,24 @@ double MPCPolicy::getDt() {return mpc.dt; }
 double MPCPolicy::getLf() {return mpc.Lf; }
 
 vector<double> MPCPolicy::step(vector<vector<double>> &nextWaypoints, vector<double> &currentStates) {
-    // Read next waypoints
-    vector<double> ptsx = nextWaypoints[0]; // x vals
-    vector<double> ptsy = nextWaypoints[1]; // y vals
     // Read state information
     double px = currentStates[0]; // x coord?
     double py = currentStates[1]; // y coord?
     double psi = currentStates[2]; // theta?
     double v = currentStates[3]; // velocity?
     // Read last action
-    double a = this->action[0]; // throttle        
+    double a = this->action[0]; // throttle
     double delta = this->action[1]; // steering?
 
     // Convert to local frame coord system
-    const size_t num_of_waypoints = ptsx.size();     
+    const size_t num_of_waypoints = nextWaypoints.size();
     Eigen::VectorXd waypoints_x_carcoord(num_of_waypoints);
     Eigen::VectorXd waypoints_y_carcoord(num_of_waypoints);
     for (size_t i = 0; i < num_of_waypoints; i++) {
-        double diff_x = ptsx[i] - px; 
-        double diff_y = ptsy[1] - py;
-        waypoints_x_carcoord[i] = diff_x * cos(-psi) - diff_y * sin(-psi); 
-        waypoints_y_carcoord[i] = diff_y * cos(-psi) - diff_x * sin(-psi); 
+        double diff_x = nextWaypoints[i][0] - px;
+        double diff_y = nextWaypoints[i][1] - py;
+        waypoints_x_carcoord[i] = diff_x * cos(-psi) - diff_y * sin(-psi);
+        waypoints_y_carcoord[i] = diff_y * cos(-psi) - diff_x * sin(-psi);
     }
 
     // Fit polynomial to waypoints
@@ -68,22 +65,21 @@ vector<double> MPCPolicy::step(vector<vector<double>> &nextWaypoints, vector<dou
     double epsi = -atan(new_coeffs[1]); // current heading error
 
     // Set up the state vector for MPC solver
-    double next_px = 0.0 + v * mpc.dt; 
-    double next_py = 0.0; 
-    double next_psi = 0.0 - v * delta / mpc.Lf * mpc.dt; 
+    double next_px = 0.0 + v * mpc.dt;
+    double next_py = 0.0;
+    double next_psi = 0.0 - v * delta / mpc.Lf * mpc.dt;
     double next_v = v + a * mpc.dt;
     double next_cte = cte + v * sin(epsi) * mpc.dt;
-    double next_epsi = epsi + v * (-delta) / mpc.Lf * mpc.dt; 
+    double next_epsi = epsi + v * (-delta) / mpc.Lf * mpc.dt;
     Eigen::VectorXd mpc_state(mpc.num_of_states);
-    mpc_state << next_px, next_py, next_psi, next_v, next_cte, next_epsi; 
-    
+    mpc_state << next_px, next_py, next_psi, next_v, next_cte, next_epsi;
+
     // Solve the MPC problem and get the solution
-    vector<double> solution = mpc.Solve(mpc_state, new_coeffs); 
-    double steering = solution[0] / deg2rad(25); 
-    double throttle = solution[1]; 
+    vector<double> solution = mpc.Solve(mpc_state, new_coeffs);
+    double steering = solution[0] / deg2rad(25);
+    double throttle = solution[1];
 
     // Update action and return it
     this->action = {throttle, steering};
     return this->action;
 }
-
